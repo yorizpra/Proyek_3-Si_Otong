d3eca6196b6b8c6a4a9a8072bc4148c41bdaac6d
Yoga Rizki Pratama <yogarizkipratama@gmail.com>
2021-05-24 18:00:43 +0700

    Midtrans

--- app/Http/Controllers/user/OrderController.php
+++ app/Http/Controllers/user/OrderController.php
@@ -7,8 +7,13 @@ use Illuminate\Http\Request;
 use App\Order;
 use App\Detailorder;
 use App\Rekening;
+use App\Product;
+use App\User;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\MidtransController;
+use App\Http\Controllers\Midtrans\Config;
+use App\Http\Controllers\Midtrans\Snap;
 class OrderController extends Controller
 {
 
@@ -128,34 +133,60 @@ class OrderController extends Controller
         if($cek_invoice < 1){
             $userid = \Auth::user()->id;
             //jika pelanggan memilih metode cod maka insert data yang ini
-        if($request->metode_pembayaran == 'cod'){
-            Order::create([
+
+            Config::$serverKey = env('MIDTRANS_SERVER_KEY');
+            Config::$isProduction = env('MIDTRANS_IS_PRODUCTION');
+            Config::$isSanitized = env('MIDTRANS_IS_SANITIZED');
+            Config::$is3ds = env('MIDTRANS_IS_3DS');
+            // jika memilih transfer maka data yang ini
+            $save=Order::create([
                 'invoice' => $request->invoice,
                 'user_id' => $userid,
                 'subtotal'=> $request->subtotal,
                 'status_order_id' => 1,
-                'metode_pembayaran' => $request->metode_pembayaran,
-                'ongkir' => $request->ongkir,
-                'biaya_cod' => 10000,
-                'no_hp' => $request->no_hp,
-                'pesan' => $request->pesan
-            ]);
-        }else{
-            //jika memilih transfer maka data yang ini
-            Order::create([
-                'invoice' => $request->invoice,
-                'user_id' => $userid,
-                'subtotal'=> $request->subtotal,
-                'status_order_id' => 1,
-                'metode_pembayaran' => $request->metode_pembayaran,
+                'metode_pembayaran' => 'transfer',
                 'ongkir' => $request->ongkir,
                 'no_hp' => $request->no_hp,
                 'pesan' => $request->pesan
             ]);
+
+            // Membuat Transaksi Midtrans
+        $user = User::where('id', \Auth::user()->id)->first();
+
+        $transaction_details = [
+            'order_id' => $save->invoice,
+            'gross_amount' => $save->subtotal, // no decimal allowed for creditcard
+        ];
+
+        $customer_details = [
+            'first_name'    => \Auth::user()->name,
+            'email'         => $user->email,
+            'phone'         => $request->no_hp,
+        ];
+
+        $enable_payments = ["credit_card", "cimb_clicks", "bca_klikbca",
+            "bca_klikpay", "bri_epay", "echannel", "permata_va",
+            "bca_va", "bni_va", "bri_va", "other_va", "gopay",
+            "indomaret", "danamon_online", "akulaku", "shopeepay"];
+
+        $transactionMidtrans = [
+            'enabled_payments' => $enable_payments,
+            'transaction_details' => $transaction_details,
+            'customer_details' => $customer_details,
+        ];
+
+        try {
+            $url_transaction = Snap::createTransaction($transactionMidtrans)->redirect_url;
+
+        }catch (\Exception $e) {
+            dd($e->getMessage());
+
         }
+    }
+
 
         $order = DB::table('order')->where('invoice',$request->invoice)->first();
-        
+
         $barang = DB::table('keranjang')->where('user_id',$userid)->get();
         //lalu masukan barang2 yang dibeli ke table detail order
         foreach($barang as $brg){
@@ -167,11 +198,6 @@ class OrderController extends Controller
         }
         //lalu hapus data produk pada keranjang pembeli
         DB::table('keranjang')->where('user_id',$userid)->delete();
-        return redirect()->route('user.order.sukses');
-        }else{
-            return redirect()->route('user.keranjang');
-        }
-        // dd($request);
-
+        return redirect($url_transaction);
     }
 }
